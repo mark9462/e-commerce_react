58c083bfa2d05036f49f7c16c53683124d61dded
Jimmy Zhang <jack9462@gmail.com>
2020-02-13 21:56:44 +0800

    Added files for home page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from './homepage.component';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
