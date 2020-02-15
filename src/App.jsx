7a5a9f4de6b3bc801fd6dfbde1a51b69bc8d4c06
Jimmy <jack9462@gmail.com>
2020-02-15 11:03:33 +0800

    creating signin page and sign in components, form-input, custom-button and signin components

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, HashRouter } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 function App() {
   return (
     <HashRouter>
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </HashRouter>
   );
