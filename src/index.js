080ac7ba66297ac24753d72ff034e995d22c0524
Jimmy <jack9462@gmail.com>
2020-02-20 14:15:32 +0800

    added redux, and implemented userReducer and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import store from './redux/store';
 
-render(<App />, document.getElementById('app'));
+render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById('app')
+);
